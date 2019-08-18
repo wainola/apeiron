7089f9c6944d9ab9c3cb22a4762465c6cd2d6fa0
Nicolás Riquelme <nrriquelme@gmail.com>
2019-08-18 13:03:01 -0400

    WIP: queryBuilder: basic unit testing

--- src/__tests__/QueryBuilderProxy.test.js
+++ src/__tests__/QueryBuilderProxy.test.js
@@ -47,7 +47,7 @@ describe('QueryBuilder test', () => {
     expect(util.types.isProxy(clientProxied)).toBe(true);
     expect(util.types.isProxy(invoiceProxied)).toBe(true);
   });
-  it.only('should build a insertion query if the proxied class use the insert method', async () => {
+  it('should build a insertion query if the proxied class use the insert method', async () => {
     const address = new Address();
     const client = new Client();
     const customer = new Customer();
@@ -61,11 +61,43 @@ describe('QueryBuilder test', () => {
       email: 'john@doe.com',
       fk_address_id: 1
     });
-    console.log('insertQuery', insertionQuery);
     const exps = `insert into client (name, lastname, email, fk_address_id) values ('john', 'doe', 'john@doe.com', '1') returning *;`
     expect(insertionQuery.toLowerCase()).toEqual(exps)
   });
-  it('should build a updation query if the proxied class use the update method', () => {});
-  it('should build a deletion query if the proxied class use the delete method', () => {});
-  it('should build a selection query if the proxied class use the get method', () => {});
+  it('should build a updation query if the proxied class use the update method', async () => {
+    const address = new Address();
+    const client = new Client();
+    const customer = new Customer();
+    const invoice = new Invoice();
+
+    const queryBuilder = new QueryBuilderProxy([address, client, customer, invoice]);
+    const customerProxied = queryBuilder.setProxy('customer')
+    const updationQuery = await customerProxied.update({name: 'Don', PDI: '12345-7'}, 23)
+    const exps = `update customer set name='don', pdi='12345-7' where id = '23';`
+    expect(updationQuery.toLowerCase()).toEqual(exps)
+  });
+  it('should build a deletion query if the proxied class use the delete method', async () => {
+    const address = new Address();
+    const client = new Client();
+    const customer = new Customer();
+    const invoice = new Invoice();
+
+    const queryBuilder = new QueryBuilderProxy([address, client, customer, invoice]);
+    const invoiceProxied = queryBuilder.setProxy('invoice')
+    const deletionQuery = await invoiceProxied.delete('24')
+    const exps = `delete from invoice where id = '24';`
+    expect(deletionQuery.toLowerCase()).toEqual(exps)
+  });
+  it('should build a selection query if the proxied class use the get method', async () => {
+    const address = new Address();
+    const client = new Client();
+    const customer = new Customer();
+    const invoice = new Invoice();
+
+    const queryBuilder = new QueryBuilderProxy([address, client, customer, invoice]);
+    const addressProxied = queryBuilder.setProxy('address')
+    const selectionQuery = await addressProxied.get(['lat', 'long'], 1001)
+    const exps = `select lat, long from address where id = '1001';`
+    expect(selectionQuery.toLowerCase()).toEqual(exps)
+  });
 });
