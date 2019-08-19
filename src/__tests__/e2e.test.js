38efef83a1a95e6c785c1e2841a1b7efa3403f30
Nicolás Riquelme <nrriquelme@gmail.com>
2019-08-19 00:38:35 -0400

    E2E testing

---
+++ src/__tests__/e2e.test.js
@@ -0,0 +1,9 @@
+const Injector = require('../Injector');
+
+describe('e2e', () => {
+  it('should return a proxied instance', () => {});
+  it('should return a insertion query on the proxied instance', () => {});
+  it('should return a updation query on the proxied instance', () => {});
+  it('should return a deletion query on the proxied instance', () => {});
+  it('should return a selection query on the proxied instance', () => {});
+});
