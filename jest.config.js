923789b06f9b06eb478c9f5d93aeb9ccfad9389d
Nicolás Riquelme <nrriquelme@gmail.com>
2019-08-19 19:35:29 -0400

    Coverage for jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'apeiron',
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
+  }
+};
